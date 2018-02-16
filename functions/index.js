62b6213301c143f9cbfac2fbc810e44b94b32d65
kimshuye <suntisuk.go@gmail.com>
2018-02-16 19:43:00 +0700

    add file public/404.html

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 
-app.get('/time',(request,response) => {
+app.get('/',(request,response) => {
     response.send(`${Date.now()}`);
 });
 
